57c92f3f6c52860e50d2293adb0d6055ab947088
junghun_kim <vienna453@gmail.com>
2025-02-27 22:20:49 +0900

    [feat] SocketIO event 추가
    - 'enter_room' event 추가
    - home에 input, button을 갖는 form 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,15 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = document.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log(`server is done!`);
+  });
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);

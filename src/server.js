8a74696148f502e6720f4dd83fedfb900b7f6610
junghun_kim <vienna453@gmail.com>
2025-03-10 19:27:48 +0900

    [feat] admin panel 세팅
    - socket io admin panel 세팅

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import http from "http";
 import SocketIO from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -13,7 +15,17 @@ app.get(`/*`, (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+  mode: "development",
+});
 
 function publicRooms() {
   const {
